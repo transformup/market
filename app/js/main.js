c54bb36c16ff730b851b58c6b13d2b5cfded9738
Vladimir <vkolouppercase@gmail.com>
2020-06-15 15:46:56 +0300

    added product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
+$(document).ready(function () {
+    $('.product-slider__wrapper').slick({
+        autoplay: false,
+        arrows: true,
+        appendArrows: $('.arrows-wrap'),
+        prevArrow: '<button id="prev" type="button" class="btn btn-juliet"></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-juliet"></button>'
+    });
 
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "15px",
+        readOnly: true
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ let gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    cssmin = require ('gulp-cssmin');
+    cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function(){
    return gulp.src('app/scss/**/*.scss')
@@ -22,7 +22,8 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -32,7 +33,8 @@ gulp.task('style', function(){
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
